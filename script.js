f4d70bd3f7238986a8d85970353be4f900781324
shukina7olga <olga.shchukina.99@mail.ru>
2026-09-18 01:00:36 +0300

    add running square

--- script.js
+++ script.js
@@ -1,28 +1,21 @@
-const DomElement = function (selector, height, width, bg, fontSize) {
+const DomElement = function (selector, height, width, bg, fontSize, position) {
     this.selector = selector,
     this.height = height,
     this.width = width,
     this.bg = bg,
-    this.fontSize = fontSize 
-    
-    //можно так создавать, но лучше через прототип
-    // this.method = function name(params) {}
+    this.fontSize = fontSize,
+    this.position = position
 }
 
-//новый метод лучше вешать на прототип, так как выигрываем
-//в производительности. но есть минус:
-//нельзя юзать скрытые переменные (внутри конструктора объявленные)
 DomElement.prototype.createElem = function () {
     const body = document.querySelector('body');
     let newElem;
     if (this.selector[0] === '.') {
         newElem = document.createElement('div');
         newElem.className = this.selector.slice(1);
-        body.append(newElem);
     } else if (this.selector[0] === '#') {
         newElem = document.createElement('p');
         newElem.id = this.selector.slice(1);
-        body.append(newElem);
     }
 
     newElem.style.cssText = `
@@ -30,13 +23,35 @@ DomElement.prototype.createElem = function () {
         width: ${this.width};
         background: ${this.bg};
         font-size: ${this.fontSize};
+        position: ${this.position};
     `;
 
     newElem.textContent = `Всем котикам фронтендерам мяу!`;
+
+    body.append(newElem);
+
+    return newElem;
 }
 
-const elem1 = new DomElement('.block', '100px', '255px', 'red', '14px');
-const elem2 = new DomElement('#best', '200px', '300px', 'green', '24px');
+document.addEventListener('DOMContentLoaded', () => {
+    const square = new DomElement('.block', '100px', '100px', 'red', '14px', 'absolute');
+    const squareElem = square.createElem();
 
-elem1.createElem();
-elem2.createElem();
+    squareElem.style.top = '0px';
+    squareElem.style.left = '0px';
+
+    document.addEventListener('keydown', (e) => {
+        let top = parseInt(squareElem.style.top);
+        let left = parseInt(squareElem.style.left);
+
+        if (e.key === 'ArrowUp') {
+            squareElem.style.top = `${top - 10}px`
+        } else if (e.key === 'ArrowDown') {
+            squareElem.style.top = `${top + 10}px`
+        } else if (e.key === 'ArrowLeft') {
+            squareElem.style.left = `${left - 10}px`
+        } else if (e.key === 'ArrowRight') {
+            squareElem.style.left = `${left + 10}px`
+        }
+    })
+})
